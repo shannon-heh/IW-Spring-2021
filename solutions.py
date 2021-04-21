90d6acae5c54bb38887137d811774eafc5269974
Shannon Heh <shannon.heh@princeton.edu>
2021-04-21 01:51:41 -0400

    Submission

--- solutions.py
+++ solutions.py
@@ -1,7 +1,30 @@
 # Depth-first search
 def dfs(node, explored):
-    pass
+    if node not in explored:
+        explored.append(node)
+        for neighbor in node.neighbors:
+            dfs(neighbor, explored)
+
+    return explored
 
 # Breadth-first search
+
+
 def bfs(start, goal):
-    pass
+    explored = []
+    queue = []
+    start_path = []
+    start_path.append(start)
+    queue.append(start_path)
+
+    while (len(queue) > 0):
+        first_path = queue.pop(0)
+        last_node = first_path[len(first_path)-1]
+        if last_node not in explored:
+            for neighbor in last_node.neighbors:
+                new_path = list(first_path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+                if neighbor == goal:
+                    return new_path
+            explored.append(last_node)
